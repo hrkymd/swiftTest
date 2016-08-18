b693f2115f35603f3e472c7e476ea9fe6b1d81fd
Yamada <defdrhrk602@gmail.com>
2016-08-19 00:14:11 +0900

    Initial Commit

---
+++ SwiftPractice/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftPractice
+//
+//  Created by Yamada on 2016/08/19.
+//  Copyright © 2016年 ymdymd. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
